6d5455d986f881dbfdb740e937caf31d41f8f0a4
juric70 <andjela.juric12@gmail.com>
2024-02-26 11:52:27 +0100

    Fix missing tasks

--- Zadatak8.cc
+++ Zadatak8.cc
@@ -1,53 +1,40 @@
 /*
-Razmak između uzastopnih prostih brojeva 2 i 3 je samo 1, dok je razmak između uzastopnih prostih brojeva 7 i 11 je 4. Napiši paralelni 
-program za određivanje svih cijelih brojeva manjih od 1.000.000, sa najvećim razmakom između para uzastopnih prostih
-brojeva. Napišite program koji će izvršiti naveden postupak u C++
-programskom jeziku.
+Napisati program u kojem će svi procesi generirati jedan nasumičan broj,
+nakon čega će jedan od procesa načiniti aritmetiču sredinu brojeva, koristeći
+operaciju redukcije, te će dobivenu aritmetičku srednju vrijednost poslati
+svim ostalim procesima. Procesi će nakon toga izračunati odstupanje u postocima
+u odnosu na dobivenu srednju vrijednost.
+
+Koristiti isključivo kolektivnu komunikaciju. Program napisati isključivo
+korištenjem C++ funkcija.
 */
 
 #include <iostream>
-#include <vector>
+#include <stdlib.h>
 #include <mpi.h>
 
 using namespace std;
 
+int main (int argc, char* argv[]) {
 
-vector<int> func(int num){
-    vector<int> primes;
-    vector<bool> isPrime(num + 1, true);
-    for (int i = 2; i <= num; i++){
-        if (isPrime[i]){
-            primes.push_back(i);
-            for (int j = 2 * i; j <= num; j += i){
-                isPrime[j] = false;
-            }
-        }
-    }
-    return primes;
-}
-
-int main(int argc, char* argv[]) 
-{
-    int rank, size;
     MPI::Init(argc, argv);
+    int rank;
+    int size;
+    double c, d;
+    float prosjek = 0;
     rank = MPI::COMM_WORLD.Get_rank();
     size = MPI::COMM_WORLD.Get_size();
-    int n = 1000000;
 
-    vector<int> primes = func(n);
+    srand((unsigned)time(NULL)+rank*size+rank);
+    c = rand()%100;
 
-    int maxGap = 0;
-    for (int i = 1; i < primes.size(); i++){
-        int gap = primes[i] - primes[i - 1];
-        maxGap = max(maxGap, gap);
-    }
+    MPI::COMM_WORLD.Reduce(&c,&d,1,MPI::DOUBLE,MPI::SUM,0);
+    if(rank == 0){
+		prosjek=(float)d/size;
+	}
 
-    int globalMaxGap;
-    MPI::COMM_WORLD.Reduce(&maxGap, &globalMaxGap, 1, MPI::INT, MPI::MAX, 0);
-
-    if (rank == 0){
-       cout << "Najveci razmak izmedju dva uzastopna prosta broja manja od: " << n << " je " << globalMaxGap << endl;
-    }
+    MPI::COMM_WORLD.Bcast(&prosjek, 1, MPI::FLOAT, 0);
+    cout<<"Proces rang: " << rank << " generirani random broj je: " << c << " prosjek je: " << prosjek << " odstupanje je: " << abs(100-(c/prosjek*100)) << "% " << endl;
     MPI::Finalize();
     return 0;
 }
